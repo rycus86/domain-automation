8dd37a33afdae4a9d26ac76976fe84ceb8877192
Viktor Adam <rycus86@gmail.com>
2018-02-08 18:29:19 +0000

    Dynamic factories [WIP]

--- src/factories.py
+++ src/factories.py
@@ -1,5 +1,15 @@
-def get_scheduler():
+import os
+
+
+scheduler_class = os.environ.get('SCHEDULER_CLASS', 'scheduler.noop.NoopScheduler')
+
+
+def _instantiate(class_name):
     pass
+
+
+def get_scheduler():
+    return _instantiate(scheduler_class)
 
 
 def get_discovery():
